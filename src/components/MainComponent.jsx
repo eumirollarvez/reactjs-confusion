ef29eb1f4379777ac190d18aaa844151c3070be9
Eumir Ollarvez <eumirollarvez@gmail.com>
2019-01-22 00:05:41 -0300

    About us Component

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -9,6 +9,7 @@ import Home from './HomeComponent';
 import Footer from './FooterComponent';
 import Header from './HeaderComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -41,6 +42,7 @@ class Main extends Component {
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
           <Route exact path='/contactus' component={Contact} />} />
+          <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
